de6c595c24eb46e0f6428419b93d1ac2d0ec3c32
Kristian Hellang <kh@novorender.com>
2025-01-22 16:34:26 +0100

    Add license headers

--- Speckle.DoubleNumerics/Vector3_Converter.cs
+++ Speckle.DoubleNumerics/Vector3_Converter.cs
@@ -1,4 +1,8 @@
-﻿#if NET6_0_OR_GREATER
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if NET6_0_OR_GREATER
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
